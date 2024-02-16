4f14cfd2a81f4f3bc64596bd1aa77546c5212307
pablosga <pablosgabussi123@gmail.com>
2024-02-16 07:28:17 -0300

    Agrega la funcion de randomizar los colores

--- script.js
+++ script.js
@@ -17,19 +17,23 @@ function createGrid(pixelsNumber) {
     }
     if (bordes.checked) eliminarBordes()
     else agregarBordes()
+    
+    pintaCuadros();
+}
 
+function pintaCuadros() {
     let cuadraditos = Array.from(document.querySelectorAll(".cuadrado"));
     cuadraditos.forEach((cuadradito) => cuadradito.addEventListener("mouseover", function(e) {
         if (bordes.checked) {
-            e.toElement.style.backgroundColor = "white";
-            e.toElement.style.border = "0.666667px solid transparent";
+            e.toElement.style.backgroundColor = actualColor();
+            eliminarBordes();
         } else {
-            e.toElement.style.backgroundColor = "white";
-            e.toElement.style.border = "0.666667px solid rgb(31, 31, 31)";
+            e.toElement.style.backgroundColor = actualColor();
+            agregarBordes();
         }
     }))
 }
-    
+
 
 let bordes = document.querySelector("#bordes")
 bordes.addEventListener("change", function() {
@@ -48,6 +52,47 @@ function agregarBordes() {
     cuadriculas.forEach((cuadro) => cuadro.style.border = "0.666667px solid rgb(31, 31, 31)");
 }
 
+let borrador = document.querySelector("#borrador");
+borrador.addEventListener("change", function() {
+    if (this.checked) activarBorrador()
+    else desactivarBorrador()
+})
+
+function activarBorrador() {
+    let cuadriculas = Array.from(document.querySelectorAll(".cuadrado"));
+    cuadriculas.forEach((cuadricula) => cuadrado.style.backgroundColor = "rgb(47, 47, 47");
+
+}
+
+function desactivarBorrador() {
+    let cuadriculas = Array.from(document.querySelectorAll(".cuadrado"));
+    cuadriculas.forEach((cuadricula) => cuadrado.style.backgroundColor = "white");
+
+}
+
+function randomColor() {
+    randomizer = () => (Math.floor(Math.random() * 256));
+    return `rgb(${randomizer()}, ${randomizer()}, ${randomizer()})`
+}
+
+function checkeadorBotones(id) {
+    let boton = document.querySelector(id);
+    if (boton.checked) return true
+    else return false
+}
+
+
+
+
+
+function actualColor() {
+    if (checkeadorBotones("#random")) return randomColor()
+    else return "white"
+}
+
+
 createGrid(16);
 
 input.addEventListener("input", () => createGrid(input.value));
+
+
